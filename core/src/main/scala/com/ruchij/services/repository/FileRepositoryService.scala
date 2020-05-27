e3a2e3b20cd5dfaafc0a42906b7ad92263f1e2f9
Ruchira (Richie) Jayasekara <me@ruchij.com>
2020-05-27 18:53:14 +1000

    Appending to the fs2 file stream in the FileRepositoryService

--- core/src/main/scala/com/ruchij/services/repository/FileRepositoryService.scala
+++ core/src/main/scala/com/ruchij/services/repository/FileRepositoryService.scala
@@ -1,6 +1,6 @@
 package com.ruchij.services.repository
 
-import java.nio.file.{Path, Paths}
+import java.nio.file.{Path, Paths, StandardOpenOption}
 
 import cats.effect.{Blocker, ContextShift, Sync}
 import cats.implicits._
@@ -11,17 +11,22 @@ import fs2.io.file.{readRange, writeAll, size => fileSize}
 class FileRepositoryService[F[_]: Sync: ContextShift](ioBlocker: Blocker) extends RepositoryService[F] {
 
   override def write(key: String, data: Stream[F, Byte]): Stream[F, Unit] =
-    Stream
-      .eval(FileRepositoryService.parsePath[F](key))
-      .flatMap(path => writeAll(path, ioBlocker).apply(data))
+    for {
+      path <- Stream.eval(FileRepositoryService.parsePath[F](key))
+      exists <- Stream.eval(fileExists(path))
+
+      result <- data.through {
+        writeAll(path, ioBlocker, if (exists) List(StandardOpenOption.APPEND) else List(StandardOpenOption.CREATE))
+      }
+    }
+    yield result
 
   override def read(key: String, start: Option[Long], end: Option[Long]): F[Option[Stream[F, Byte]]] =
     FileRepositoryService.parsePath[F](key)
       .flatMap { path =>
-        Sync[F]
-          .delay(path.toFile.exists())
-          .flatMap { fileExists =>
-            if (fileExists)
+        fileExists(path)
+          .flatMap { exists =>
+            if (exists)
               Sync[F].delay(
                 Some(readRange(path, ioBlocker, FileRepositoryService.CHUNK_SIZE, start.getOrElse(0), end.getOrElse(Long.MaxValue)))
               )
@@ -33,11 +38,14 @@ class FileRepositoryService[F[_]: Sync: ContextShift](ioBlocker: Blocker) extend
   override def size(key: String): F[Option[Long]] =
     for {
       path <- FileRepositoryService.parsePath[F](key)
-      fileExists <- Sync[F].delay(path.toFile.exists())
+      exists <- fileExists(path)
 
-      bytes <- if (fileExists) fileSize(ioBlocker, path).map[Option[Long]](Some.apply) else Applicative[F].pure[Option[Long]](None)
+      bytes <- if (exists) fileSize(ioBlocker, path).map[Option[Long]](Some.apply) else Applicative[F].pure[Option[Long]](None)
     }
     yield bytes
+
+  def fileExists(path: Path): F[Boolean] =
+    Sync[F].delay(path.toFile.exists())
 }
 
 object FileRepositoryService {
