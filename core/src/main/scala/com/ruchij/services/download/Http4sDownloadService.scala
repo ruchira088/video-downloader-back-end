a2a5c8950a28d5492a6a70391afc4be7ddd203ac
Ruchira (Richie) Jayasekara <me@ruchij.com>
2020-07-19 19:22:38 +1000

    Updated the Http4sDownloadService

--- core/src/main/scala/com/ruchij/services/download/Http4sDownloadService.scala
+++ core/src/main/scala/com/ruchij/services/download/Http4sDownloadService.scala
@@ -2,14 +2,15 @@ package com.ruchij.services.download
 
 import java.util.concurrent.TimeUnit
 
+import cats.Functor
 import cats.effect.{Clock, Concurrent, ContextShift, Resource}
 import cats.implicits._
-import com.ruchij.services.download.models.DownloadResult
+import com.ruchij.services.download.models.{DownloadResult, RangeFrom}
 import com.ruchij.services.repository.RepositoryService
 import com.ruchij.utils.Http4sUtils
 import org.http4s.client.Client
 import org.http4s.headers.{`Content-Length`, `Content-Type`}
-import org.http4s.{Request, Uri}
+import org.http4s.{Headers, Request, Uri}
 
 class Http4sDownloadService[F[_]: Concurrent: ContextShift: Clock](
   client: Client[F],
@@ -17,21 +18,19 @@ class Http4sDownloadService[F[_]: Concurrent: ContextShift: Clock](
 ) extends DownloadService[F] {
 
   override def download(uri: Uri, parent: String): Resource[F, DownloadResult[F]] =
-    client
-      .run(Request(uri = uri))
-      .product {
-        Resource.liftF {
-          Clock[F]
-            .realTime(TimeUnit.MILLISECONDS)
-            .map { prefix =>
-              val fileName = uri.path.split("/").lastOption.getOrElse("new-download")
-
-              parent + (if (parent.endsWith("/")) "" else "/") + s"$prefix-$fileName"
-            }
-        }
+    Resource
+      .liftF(Http4sDownloadService.filePath(uri, parent))
+      .evalMap { path =>
+        repositoryService.size(path).map(size => path -> size.getOrElse(0L))
+      }
+      .flatMap {
+        case (path, start) =>
+          client
+            .run(Request(uri = uri, headers = Headers.of(RangeFrom(start))))
+            .map(response => (response, path, start))
       }
       .evalMap {
-        case (response, key) =>
+        case (response, path, start) =>
           Http4sUtils
             .header[F](`Content-Length`)
             .product(Http4sUtils.header[F](`Content-Type`))
@@ -42,15 +41,28 @@ class Http4sDownloadService[F[_]: Concurrent: ContextShift: Clock](
             .run(response)
             .map {
               case (fileSize, mediaType) =>
-                DownloadResult.create[F](uri, key, fileSize, mediaType) {
+                DownloadResult.create[F](uri, path, fileSize, mediaType) {
                   response.body
                     .observe { data =>
-                      repositoryService.write(key, data)
+                      repositoryService.write(path, data)
                     }
                     .chunks
-                    .scan(0L) { case (total, chunk) => total + chunk.size }
+                    .scan(start) { case (total, chunk) => total + chunk.size }
                 }
             }
+      }
+
+}
+
+object Http4sDownloadService {
+
+  def filePath[F[_]: Clock: Functor](uri: Uri, parent: String): F[String] =
+    Clock[F]
+      .realTime(TimeUnit.MILLISECONDS)
+      .map { prefix =>
+        val fileName = uri.path.split("/").lastOption.getOrElse("new-download")
+
+        parent + (if (parent.endsWith("/")) "" else "/") + s"$prefix-$fileName"
       }
 
 }
