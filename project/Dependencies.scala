d3006cecf0559f7665bf20f812ae6710e3986426
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-02-27 02:30:10 +1100

    Upgrading to Scala 2.13.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies
 {
-  val ScalaVersion = "2.13.4"
+  val ScalaVersion = "2.13.5"
   val Http4sVersion = "0.21.19"
   val CirceVersion = "0.13.0"
 
@@ -42,7 +42,7 @@ object Dependencies
 
   lazy val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.14.0"
 
-  lazy val flywayCore = "org.flywaydb" % "flyway-core" % "7.5.3"
+  lazy val flywayCore = "org.flywaydb" % "flyway-core" % "7.5.4"
 
   lazy val postgresql = "org.postgresql" % "postgresql" % "42.2.19"
 
@@ -70,7 +70,7 @@ object Dependencies
 
   lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.11.3" cross CrossVersion.full
 
-  lazy val scalaTypedHoles = "com.github.cb372" % "scala-typed-holes" % "0.1.7" cross CrossVersion.full
+  lazy val scalaTypedHoles = "com.github.cb372" % "scala-typed-holes" % "0.1.8" cross CrossVersion.full
 
   lazy val betterMonadicFor = "com.olegpy" %% "better-monadic-for" % "0.3.1"
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,5 @@
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.6.1")
-addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.7.5")
+addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.8.0")
 addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.10.0")
 addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.5.1")
-addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.13")
+addSbtPlugin("com.github.sbt" % "sbt-release" % "1.0.15")
