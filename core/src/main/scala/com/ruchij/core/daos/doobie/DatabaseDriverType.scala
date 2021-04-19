d48b8b05e96f659e074074e8fc239cbaf4c79f3c
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-04-20 00:16:50 +1000

    Refactoring

--- core/src/main/scala/com/ruchij/core/daos/doobie/DatabaseDriverType.scala
+++ core/src/main/scala/com/ruchij/core/daos/doobie/DatabaseDriverType.scala
@@ -1,23 +1,44 @@
 package com.ruchij.core.daos.doobie
 
+import cats.{Applicative, ApplicativeError}
 import enumeratum.{Enum, EnumEntry}
+import org.{h2, postgresql}
 
-sealed trait DatabaseDriverType extends EnumEntry {
-  val driver: String
-  val urlName: String
+import java.sql.Driver
+import scala.reflect.ClassTag
+import scala.util.matching.Regex
+
+sealed abstract class DatabaseDriverType[A <: Driver](implicit classTag: ClassTag[A]) extends EnumEntry {
+  val driver: String = classTag.runtimeClass.getName
 }
 
-object DatabaseDriverType extends Enum[DatabaseDriverType] {
+object DatabaseDriverType extends Enum[DatabaseDriverType[_]] {
+  case object PostgreSQL extends DatabaseDriverType[postgresql.Driver]
+  case object H2 extends DatabaseDriverType[h2.Driver]
 
-  case object PostgreSQL extends DatabaseDriverType {
-    override val driver: String = "org.postgresql.Driver"
-    override val urlName: String = "postgresql"
-  }
+  val UrlName: Regex = "^jdbc:(\\w+):.*".r
 
-  case object H2 extends DatabaseDriverType {
-    override val driver: String = "org.h2.Driver"
-    override val urlName: String = "h2"
-  }
+  def parseFromConnectionUrl[F[_]: ApplicativeError[*[_], Throwable]](connectionUrl: String): F[DatabaseDriverType[_]] =
+    connectionUrl match {
+      case UrlName(name) =>
+        DatabaseDriverType.values
+          .find(_.entryName.equalsIgnoreCase(name))
+          .map(Applicative[F].pure)
+          .getOrElse {
+            ApplicativeError[F, Throwable].raiseError {
+              new IllegalArgumentException(
+                s"""Extracted DB URL name "$name" does NOT match any DatabaseDriverTypes. Possible values: [${DatabaseDriverType.values
+                  .map(_.entryName)
+                  .mkString(", ")}]"""
+              )
+            }
+          }
 
-  override def values: IndexedSeq[DatabaseDriverType] = findValues
+      case url =>
+        ApplicativeError[F, Throwable].raiseError(
+          new IllegalArgumentException(s"""Unable to infer driver from "$url"""")
+        )
+    }
+
+  override def values: IndexedSeq[DatabaseDriverType[_]] = findValues
 }
--- core/src/main/scala/com/ruchij/core/daos/doobie/DoobieTransactor.scala
+++ core/src/main/scala/com/ruchij/core/daos/doobie/DoobieTransactor.scala
@@ -1,16 +1,14 @@
 package com.ruchij.core.daos.doobie
 
 import cats.effect.{Async, Blocker, ContextShift, Resource}
-import cats.{Applicative, ApplicativeError}
+import com.ruchij.core.daos.doobie.DatabaseDriverType.parseFromConnectionUrl
 import com.ruchij.migration.config.DatabaseConfiguration
 import doobie.hikari.HikariTransactor
 import doobie.util.ExecutionContexts
 
 import scala.concurrent.ExecutionContext
-import scala.util.matching.Regex
 
 object DoobieTransactor {
-  val UrlName: Regex = "^jdbc:(\\w+):.*".r
 
   def create[F[_]: Async: ContextShift](databaseConfiguration: DatabaseConfiguration): Resource[F, HikariTransactor[F]] =
     for {
@@ -34,26 +32,4 @@ object DoobieTransactor {
             blocker
           )
       }
-
-  def parseFromConnectionUrl[F[_]: ApplicativeError[*[_], Throwable]](connectionUrl: String): F[DatabaseDriverType] =
-    connectionUrl match {
-      case UrlName(name) =>
-        DatabaseDriverType.values
-          .find(_.urlName == name)
-          .map(Applicative[F].pure)
-          .getOrElse {
-            ApplicativeError[F, Throwable].raiseError {
-              new IllegalArgumentException(
-                s"""Extracted DB URL name "$name" does NOT match any DatabaseDriverTypes. Possible values: [${DatabaseDriverType.values
-                  .map(_.urlName)
-                  .mkString(", ")}]"""
-              )
-            }
-          }
-
-      case url =>
-        ApplicativeError[F, Throwable].raiseError(
-          new IllegalArgumentException(s"""Unable to infer driver from "$url"""")
-        )
-    }
 }
