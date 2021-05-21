e85296d78bab560b238b51ff7a228caa0e9b7149
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-05-21 23:05:20 +1000

    Upgrading dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies
 
   lazy val fs2KafkaVulkan = "com.github.fd4s" %% "fs2-kafka-vulcan" % "1.6.1"
 
-  lazy val vulkanGeneric = "com.github.fd4s" %% "vulcan-generic" % "1.6.0"
+  lazy val vulkanGeneric = "com.github.fd4s" %% "vulcan-generic" % "1.7.1"
 
   lazy val circeGeneric = "io.circe" %% "circe-generic" % CirceVersion
 
@@ -44,7 +44,7 @@ object Dependencies
 
   lazy val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.15.0"
 
-  lazy val flywayCore = "org.flywaydb" % "flyway-core" % "7.8.1"
+  lazy val flywayCore = "org.flywaydb" % "flyway-core" % "7.9.1"
 
   lazy val postgresql = "org.postgresql" % "postgresql" % "42.2.20"
 
