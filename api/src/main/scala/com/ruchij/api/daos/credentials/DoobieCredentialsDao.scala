e499b29ccba08d4bbb48eb3394f916698fed985e
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-09-20 20:34:23 +1000

    Implemented DoobieCredentialsDao

--- api/src/main/scala/com/ruchij/api/daos/credentials/DoobieCredentialsDao.scala
+++ api/src/main/scala/com/ruchij/api/daos/credentials/DoobieCredentialsDao.scala
@@ -1,10 +1,21 @@
 package com.ruchij.api.daos.credentials
 
 import com.ruchij.api.daos.credentials.models.Credentials
+import com.ruchij.core.daos.doobie.DoobieCustomMappings.{dateTimeGet, dateTimePut}
 import doobie.free.connection.ConnectionIO
+import doobie.implicits.toSqlInterpolator
 
 object DoobieCredentialsDao extends CredentialsDao[ConnectionIO] {
-  override def insert(credentials: Credentials): ConnectionIO[Int] = ???
+  override def insert(credentials: Credentials): ConnectionIO[Int] =
+    sql"""
+      INSERT INTO credentials (user_id, last_updated_at, hashed_password)
+        VALUES (${credentials.userId}, ${credentials.lastUpdatedAt}, ${credentials.hashedPassword})
+    """
+      .update
+      .run
 
-  override def findCredentialsByUserId(userId: String): ConnectionIO[Option[Credentials]] = ???
+  override def findCredentialsByUserId(userId: String): ConnectionIO[Option[Credentials]] =
+    sql"SELECT user_id, last_updated_at, hashed_password FROM credentials WHERE user_id = $userId"
+      .query[Credentials]
+      .option
 }
