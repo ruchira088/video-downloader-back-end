e011b6b7e61f04c23a88948ab48999df378b076d
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-09-20 20:41:41 +1000

    Implementing DoobieUserDao

--- api/src/main/scala/com/ruchij/api/daos/user/DoobieUserDao.scala
+++ api/src/main/scala/com/ruchij/api/daos/user/DoobieUserDao.scala
@@ -1,12 +1,30 @@
 package com.ruchij.api.daos.user
 
 import com.ruchij.api.daos.user.models.{Email, User}
+import com.ruchij.core.daos.doobie.DoobieCustomMappings.{dateTimeGet, dateTimePut, enumGet, enumPut}
 import doobie.free.connection.ConnectionIO
+import doobie.implicits.toSqlInterpolator
 
 object DoobieUserDao extends UserDao[ConnectionIO] {
-  override def insert(user: User): ConnectionIO[Int] = ???
+  val SelectQuery =
+    fr"SELECT id, created_at, first_name, last_name, email, role FROM api_user"
 
-  override def findByEmail(email: Email): ConnectionIO[Option[User]] = ???
+  override def insert(user: User): ConnectionIO[Int] =
+    sql"""
+        INSERT INTO api_user (id, created_at, first_name, last_name, email, role)
+            VALUES (
+              ${user.id},
+              ${user.createdAt},
+              ${user.firstName},
+              ${user.lastName},
+              ${user.email},
+              ${user.role}
+            )    
+    """.update.run
 
-  override def findById(userId: String): ConnectionIO[Option[User]] = ???
+  override def findByEmail(email: Email): ConnectionIO[Option[User]] =
+    (SelectQuery ++ fr"WHERE email = $email").query[User].option
+
+  override def findById(userId: String): ConnectionIO[Option[User]] =
+    (SelectQuery ++ fr"WHERE id = $userId").query[User].option
 }
