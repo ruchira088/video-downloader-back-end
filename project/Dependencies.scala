1ba25f8e7e4daf382e436c6595a5450b3b5f03f4
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-11-26 17:17:46 +1100

    Upgrading to flyway version 8.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Dependencies
 
   lazy val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.17.1"
 
-  lazy val flywayCore = "org.flywaydb" % "flyway-core" % "8.0.5"
+  lazy val flywayCore = "org.flywaydb" % "flyway-core" % "8.1.0"
 
   lazy val postgresql = "org.postgresql" % "postgresql" % "42.3.1"
 
