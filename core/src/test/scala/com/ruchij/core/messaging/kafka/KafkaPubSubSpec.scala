a299f3b3506967aa57ba6065764d4129d2a500e3
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-05-01 12:39:09 +1000

    Added test for Kafka publisher and subscriber

---
+++ core/src/test/scala/com/ruchij/core/messaging/kafka/KafkaPubSubSpec.scala
@@ -0,0 +1,47 @@
+package com.ruchij.core.messaging.kafka
+
+import cats.effect.IO
+import fs2.Stream
+import com.ruchij.core.external.embedded.EmbeddedExternalServiceProvider
+import com.ruchij.core.messaging.kafka.KafkaPubSubSpec.TestMessage
+import com.ruchij.core.test.IOSupport.runIO
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers
+import vulcan.Codec
+import vulcan.generic.MagnoliaCodec
+
+class KafkaPubSubSpec extends AnyFlatSpec with Matchers {
+
+  "Kafka publisher and subscriber" should "be able to publish and subscribe to Kafka topic" in runIO {
+    new EmbeddedExternalServiceProvider[IO].kafkaConfiguration
+      .flatMap {
+        kafkaConfiguration => KafkaPubSub(kafkaConfiguration)(IO.asyncForIO, KafkaPubSubSpec.TestMessageTopic)
+      }
+      .use { pubSub =>
+        pubSub.subscribe("test-subscriber").take(10).compile.toList.start
+          .productL {
+            pubSub.publish { Stream.range[IO, Int](0, 10).map(index => TestMessage(index)) }
+              .compile
+              .drain
+          }
+          .flatMap(_.joinWithNever)
+          .flatMap { committableRecords =>
+            IO.delay {
+              committableRecords.size mustBe 10
+              committableRecords.map(_.value.index).toSet mustBe Range(0, 10).toSet
+            }
+          }
+      }
+  }
+
+}
+
+object KafkaPubSubSpec {
+  case class TestMessage(index: Int)
+
+  implicit case object TestMessageTopic extends KafkaTopic[TestMessage] {
+    override val name: String = "test-topic"
+
+    override val codec: Codec[TestMessage] = Codec.derive[TestMessage]
+  }
+}
