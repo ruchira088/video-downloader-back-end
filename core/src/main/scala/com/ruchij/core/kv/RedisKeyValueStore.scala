b7c8cc49608b30e6cbda807fbd7d6a0b3378a404
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-09-01 21:37:17 +1000

    Setting the TTL and key value in a single call on Redis

--- core/src/main/scala/com/ruchij/core/kv/RedisKeyValueStore.scala
+++ core/src/main/scala/com/ruchij/core/kv/RedisKeyValueStore.scala
@@ -1,14 +1,15 @@
 package com.ruchij.core.kv
 
-import cats.Applicative
-import cats.effect.kernel.MonadCancelThrow
 import cats.implicits._
+import cats.{Applicative, MonadThrow}
 import com.ruchij.core.kv.codecs.{KVDecoder, KVEncoder}
 import dev.profunktor.redis4cats.RedisCommands
+import dev.profunktor.redis4cats.effects.SetArgs
+import dev.profunktor.redis4cats.effects.SetArg.Ttl.Px
 
 import scala.concurrent.duration.FiniteDuration
 
-class RedisKeyValueStore[F[_]: MonadCancelThrow](redisCommands: RedisCommands[F, String, String]) extends KeyValueStore[F] {
+class RedisKeyValueStore[F[_]: MonadThrow](redisCommands: RedisCommands[F, String, String]) extends KeyValueStore[F] {
   override type InsertionResult = Unit
   override type DeletionResult = Unit
 
@@ -17,7 +18,7 @@ class RedisKeyValueStore[F[_]: MonadCancelThrow](redisCommands: RedisCommands[F,
       encodedKey <- KVEncoder[F, K].encode(key)
       fetchedValue <- redisCommands.get(encodedKey)
       value <- fetchedValue.fold[F[Option[V]]](Applicative[F].pure(None)) { stringValue =>
-        MonadCancelThrow[F].handleErrorWith[Option[V]](KVDecoder[F, V].decode(stringValue).map(Some.apply)) { _ => remove(key).as(None) }
+        MonadThrow[F].handleErrorWith[Option[V]](KVDecoder[F, V].decode(stringValue).map(Some.apply)) { _ => remove(key).as(None) }
       }
     } yield value
 
@@ -26,8 +27,7 @@ class RedisKeyValueStore[F[_]: MonadCancelThrow](redisCommands: RedisCommands[F,
       encodedKey <- KVEncoder[F, K].encode(key)
       encodedValue <- KVEncoder[F, V].encode(value)
 
-      result <- redisCommands.set(encodedKey, encodedValue)
-      _ <- maybeTtl.fold(Applicative[F].pure(false)){ ttl => redisCommands.expire(encodedKey, ttl) }
+      result <- redisCommands.set(encodedKey, encodedValue, SetArgs(None, maybeTtl.map(ttl => Px(ttl))))
     } yield result
 
   override def remove[K: KVEncoder[F, *]](key: K): F[Unit] =
