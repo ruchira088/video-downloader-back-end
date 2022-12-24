4b206a10f7a5bdee0bf5ff5eacb4c5f482b41b39
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-12-24 19:54:49 +1100

    Updating the DB restore job

--- db-restore/run-db-restore.sh
+++ db-restore/run-db-restore.sh
@@ -7,9 +7,11 @@ set -e
 : "${DATABASE_USER:? Variable NOT defined}"
 : "${DATABASE_PASSWORD:? Variable NOT defined}"
 
-echo "Downloading backup data from AWS S3"
+s3_url="s3://backups.video-downloader.ruchij.com/$BACKUP_FILE"
 
-aws s3 cp "s3://backups.video-downloader.ruchij.com/$BACKUP_FILE" backup.zip
+echo "Downloading backup data from AWS S3 $s3_url"
+
+aws s3 cp "$s3_url" backup.zip
 
 echo "Download completed from S3"
 
@@ -18,6 +20,8 @@ unzip backup.zip -d backup_contents
 database_url="postgresql://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_URL"
 
 echo "Applying backup SQL data to database"
+
+psql "$database_url" -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
 
 psql "$database_url" < backup_contents/backup.sql
 
