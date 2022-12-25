0eeaca0963428409cfd725d6679b0352a7b93efe
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-12-26 09:45:22 +1100

    Upgrading to cats-effect 3.4.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies
 
   lazy val http4sJdkHttpClient = "org.http4s" %% "http4s-jdk-http-client" % "0.8.0"
 
-  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.4.2"
+  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.4.3"
 
   lazy val kafka = "org.apache.kafka" %% "kafka" % "7.3.1-ccs"
 
