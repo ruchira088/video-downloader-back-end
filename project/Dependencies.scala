ab4950fb577bfc5cf9c4d878297a216ad2271655
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-06-12 15:55:49 +0800

    Upgrading to flyway 9.19.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Dependencies
 
   lazy val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.17.4"
 
-  lazy val flywayCore = "org.flywaydb" % "flyway-core" % "9.19.3"
+  lazy val flywayCore = "org.flywaydb" % "flyway-core" % "9.19.4"
 
   lazy val postgresql = "org.postgresql" % "postgresql" % "42.6.0"
 
