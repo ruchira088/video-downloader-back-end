f07d14037f5165f2b2d4c67818369e85ec117ad2
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-07-12 23:14:34 +1000

    Upgrading dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Dependencies
 
   lazy val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.17.4"
 
-  lazy val flywayCore = "org.flywaydb" % "flyway-core" % "9.20.0"
+  lazy val flywayCore = "org.flywaydb" % "flyway-core" % "9.20.1"
 
   lazy val postgresql = "org.postgresql" % "postgresql" % "42.6.0"
 
