74273f881cdb7986eac6150e80537f5361eb3f9f
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-06-09 17:35:51 +1000

    Setting S3 bucket expiration policy for database backups

--- terraform/backup.tf
+++ terraform/backup.tf
@@ -3,6 +3,21 @@ resource "aws_s3_bucket" "backup_bucket" {
   bucket = "backups.video-downloader.ruchij.com"
 }
 
+
+resource "aws_s3_bucket_lifecycle_configuration" "backup_bucket_lifecycle" {
+  bucket = aws_s3_bucket.backup_bucket.id
+
+  rule {
+    id = "expiration"
+
+    expiration {
+      days = 365
+    }
+
+    status = "Enabled"
+  }
+}
+
 resource "aws_iam_user" "backup_creator" {
   name = "video-downloader-backup-creator"
 }
