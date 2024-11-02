2a472d613786b654b9d6a6bee16f1300467bbee1
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-11-02 20:51:38 +1100

    Upgrading pip dependencies

--- fallback-api/src/web/blueprints/service_blueprint.py
+++ fallback-api/src/web/blueprints/service_blueprint.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Blueprint, Response, jsonify
 
 service_blueprint = Blueprint('service', __name__, url_prefix='/service')
@@ -5,5 +7,7 @@ service_blueprint = Blueprint('service', __name__, url_prefix='/service')
 @service_blueprint.route('/info')
 def info():
     return jsonify({
+        'service_name': 'video-downloader-fallback-api',
+        'timestamp': datetime.now().isoformat(),
         'status': 'ok',
     })
