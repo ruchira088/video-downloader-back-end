d33ca3f0638df34c614851e4a693543abe7ae4f9
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-11-10 20:57:33 +1100

    Removing marshmallow data validation

--- fallback-api/src/web/blueprints/schedule_blueprint.py
+++ fallback-api/src/web/blueprints/schedule_blueprint.py
@@ -1,15 +1,10 @@
-from flask import Blueprint, request, jsonify
-from marshmallow import Schema, fields, EXCLUDE
+from flask import Blueprint
 
 from src.web.decorators.authentication import authenticated
 
 schedule_blueprint = Blueprint('schedule', __name__, url_prefix='/schedule')
 
-class VideoDownloadRequestSchema(Schema):
-    videoUrl = fields.URL(required=True)
-
-@schedule_blueprint.post('/')
+@schedule_blueprint.route('/', methods=['POST'])
 @authenticated
 def schedule_video_download():
-    video_download_request = VideoDownloadRequestSchema().load(request.json, unknown=EXCLUDE)
-    return jsonify(video_download_request)
+    pass
