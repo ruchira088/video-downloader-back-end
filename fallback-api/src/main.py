55ad114aa4b9643afaede403b36633afa718a6f4
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-11-30 22:06:25 +1100

    Upgrading to sbt 1.10.6

--- fallback-api/src/main.py
+++ fallback-api/src/main.py
@@ -5,12 +5,17 @@ from src.web.blueprints.service_blueprint import service_blueprint
 from src.web.blueprints.user_blueprint import user_blueprint
 from src.web.blueprints.video_blueprint import video_blueprint
 
-app = Flask(__name__)
+def create_http_app():
+    app = Flask(__name__)
 
-app.register_blueprint(user_blueprint)
-app.register_blueprint(schedule_blueprint)
-app.register_blueprint(video_blueprint)
-app.register_blueprint(service_blueprint)
+    app.register_blueprint(user_blueprint)
+    app.register_blueprint(schedule_blueprint)
+    app.register_blueprint(video_blueprint)
+    app.register_blueprint(service_blueprint)
+
+    return app
+
+http_app = create_http_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    create_http_app().run(debug=True)
