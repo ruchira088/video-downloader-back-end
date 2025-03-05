18a6286e2d0fdf7918aaf6e8138e049a28d5a2ce
Ruchira (Richie) Jayasekara <me@ruchij.com>
2025-03-05 12:09:26 +1100

    Upgrading dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies
   val ScalaVersion = "2.13.16"
   val Http4sVersion = "0.23.30"
   val CirceVersion = "0.14.10"
-  val TestContainersVersion = "1.20.5"
+  val TestContainersVersion = "1.20.6"
   val FlywayVersion = "11.3.4"
 
   lazy val http4sDsl = "org.http4s" %% "http4s-dsl" % Http4sVersion
