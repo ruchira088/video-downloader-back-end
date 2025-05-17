c636ccdad8483bee9511a6a1b9a0981c0bd14c84
Ruchira (Richie) Jayasekara <me@ruchij.com>
2025-05-18 01:46:52 +1000

    Refactor FrontEndContainer to remove apiUri parameter and update port configuration

--- development/src/main/scala/com/ruchij/development/frontend/FrontEndContainer.scala
+++ development/src/main/scala/com/ruchij/development/frontend/FrontEndContainer.scala
@@ -4,33 +4,30 @@ import cats.effect.kernel.{Resource, Sync}
 import cats.implicits._
 import com.ruchij.core.external.containers.ContainerCoreResourcesProvider
 import com.ruchij.core.types.FunctionKTypes.{FunctionK2TypeOps, _}
-import com.ruchij.development.frontend.FrontEndContainer.ApiUrlEnvVar
 import org.http4s.Uri
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.wait.strategy.Wait
 
 import scala.jdk.CollectionConverters.SeqHasAsJava
 
-class FrontEndContainer(apiUri: Uri)
-    extends GenericContainer[FrontEndContainer]("ghcr.io/ruchira088/video-downloader-front-end-dev:dev") {
+class FrontEndContainer
+    extends GenericContainer[FrontEndContainer]("ghcr.io/ruchira088/video-downloader-front-end:dev") {
   setWaitStrategy(Wait.forHttp("/"))
   setExposedPorts(List(FrontEndContainer.Port: Integer).asJava)
-  addEnv(ApiUrlEnvVar, apiUri.renderString)
 }
 
 object FrontEndContainer {
-  private val Port = 3000
-  private val ApiUrlEnvVar = "REACT_APP_API_URL"
+  private val Port = 80
 
   def create[F[_]: Sync](apiUrl: Uri): Resource[F, Uri] =
     Resource
-      .eval(Sync[F].delay(new FrontEndContainer(apiUrl)))
+      .eval(Sync[F].delay(new FrontEndContainer()))
       .flatMap(frontEndContainer => ContainerCoreResourcesProvider.start(frontEndContainer))
       .evalMap { frontEndContainer =>
         for {
           host <- Sync[F].delay(frontEndContainer.getHost)
           port <- Sync[F].delay(frontEndContainer.getMappedPort(Port))
-          uri <- Uri.fromString(s"http://$host:$port").toType[F, Throwable]
+          uri <- Uri.fromString(s"http://$host:$port?API_URL=${apiUrl.renderString}").toType[F, Throwable]
         } yield uri
       }
 }
