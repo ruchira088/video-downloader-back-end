767078cb45e3ae8a67840a559166610275a6cc33
Ruchira (Richie) Jayasekara <me@ruchij.com>
2025-05-24 21:49:44 +1000

    Introduce dual CORS policies for selective request handling
    Added a primary CORS policy for specified allowed hosts and an open CORS policy for predefined routes. Modified the `apply` method to dynamically determine which policy to apply based on the request path and method. Simplifies handling of public API routes while maintaining restricted access elsewhere.

--- api/src/main/scala/com/ruchij/api/web/middleware/Cors.scala
+++ api/src/main/scala/com/ruchij/api/web/middleware/Cors.scala
@@ -1,20 +1,50 @@
 package com.ruchij.api.web.middleware
 
+import cats.Applicative
+import cats.data.Kleisli
+import org.http4s.Method.{GET, OPTIONS, POST}
+import org.http4s.Uri.Path
+import org.http4s.Uri.Path.Root
 import org.http4s.server.middleware.{CORS, CORSPolicy}
+import org.http4s.{HttpApp, Method, Request, Response}
 
 object Cors {
+  private val OpenCorsPolicyRequests: Set[(Method, Path)] =
+    Set(GET -> Root / "schedule" / "search", POST -> Root / "schedule", POST -> Root / "videos" / "metadata")
+
   private def hostRegex(host: String): Set[String] =
     Set(host)
       .flatMap(origin => if (origin.startsWith("*.")) Set(origin.substring(2), origin) else Set(origin))
       .map(origin => s"https?://${origin.replace(".", "\\.").replace("*", ".*")}(:\\d+)?$$")
 
-  def apply(allowedHosts: Set[String]): CORSPolicy = {
+  private def isOpenCorsPolicyRequest[F[_]](request: Request[F]): Boolean =
+    OpenCorsPolicyRequests
+      .concat(OpenCorsPolicyRequests.map { case (_, path) => OPTIONS -> path })
+      .contains((request.method, request.pathInfo))
+
+  def apply[F[_]: Applicative](allowedHosts: Set[String])(httpApp: HttpApp[F]): HttpApp[F] = {
     val hostsRegex = allowedHosts.flatMap(hostRegex).mkString("|").r
 
-    CORS.policy
-      .withAllowCredentials(true)
-      .withAllowOriginHostCi { originHost =>
-        hostsRegex.matches(originHost.toString)
+    val primaryCorsPolicy: CORSPolicy =
+      CORS.policy
+        .withAllowCredentials(true)
+        .withAllowOriginHostCi { originHost =>
+          hostsRegex.matches(originHost.toString)
+        }
+
+    val openCorsPolicy: CORSPolicy =
+      CORS.policy
+        .withAllowCredentials(true)
+        .withAllowOriginHost { _ =>
+          true
+        }
+
+    Kleisli[F, Request[F], Response[F]] { request =>
+      if (isOpenCorsPolicyRequest(request)) {
+        openCorsPolicy(httpApp).run(request)
+      } else {
+        primaryCorsPolicy(httpApp).run(request)
       }
+    }
   }
 }
