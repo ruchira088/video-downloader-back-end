a358a58ca60b3b59f5317c994d10d9b9190fcc95
Ruchira (Richie) Jayasekara <me@ruchij.com>
2025-06-15 15:25:02 +1000

    Update VideoScanProgressResponse to include updatedAt field

--- api/src/main/scala/com/ruchij/api/web/responses/VideoScanProgressResponse.scala
+++ api/src/main/scala/com/ruchij/api/web/responses/VideoScanProgressResponse.scala
@@ -3,4 +3,4 @@ package com.ruchij.api.web.responses
 import com.ruchij.core.daos.workers.models.VideoScan.ScanStatus
 import org.joda.time.DateTime
 
-final case class VideoScanProgressResponse(startedAt: Option[DateTime], scanStatus: ScanStatus)
+final case class VideoScanProgressResponse(updatedAt: Option[DateTime], scanStatus: ScanStatus)
