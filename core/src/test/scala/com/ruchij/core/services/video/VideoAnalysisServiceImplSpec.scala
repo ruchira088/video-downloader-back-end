f25c2e5a4c9a2c8e13b63ebdbbc172f5466e2f73
Ruchira (Richie) Jayasekara <me@ruchij.com>
2025-09-06 21:10:49 +1000

    Fix video thumbnail URI in VideoAnalysisServiceImplSpec

--- core/src/test/scala/com/ruchij/core/services/video/VideoAnalysisServiceImplSpec.scala
+++ core/src/test/scala/com/ruchij/core/services/video/VideoAnalysisServiceImplSpec.scala
@@ -86,7 +86,7 @@ class VideoAnalysisServiceImplSpec extends AnyFlatSpec with MockFactory with Mat
           videoAnalysisResult.title mustBe "BREAKING WHITE BLONDE BOOTY - GISELLE PALMER"
           videoAnalysisResult.duration mustBe ((37 minutes) + (1 seconds))
           videoAnalysisResult.size mustBe 584122827
-          videoAnalysisResult.thumbnail mustBe uri"https://image.xfreehd.com/media/videos/tmb10/343591/1b.jpg"
+          videoAnalysisResult.thumbnail mustBe uri"https://xfreehd.com/media/videos/tmb10/343591/1b.jpg"
           videoAnalysisResult.videoSite mustBe CustomVideoSite.XFreeHD
         }
     }.value
