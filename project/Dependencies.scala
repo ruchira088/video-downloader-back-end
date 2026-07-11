cd42e398a4983ab2894dd57cd79fac5c1e418cd6
Ruchira (Richie) Jayasekara <me@ruchij.com>
2026-07-11 23:33:47 +1000

    Bump dependencies: Http4s to 0.23.36, Flyway to 12.11.0, PostgreSQL to 42.7.13, Logback to 1.5.38, and Sentry to 8.48.0.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,9 +3,9 @@ import sbt.*
 object Dependencies
 {
   val ScalaVersion = "2.13.18"
-  private val Http4sVersion = "0.23.34"
+  private val Http4sVersion = "0.23.36"
   private val CirceVersion = "0.14.16"
-  private val FlywayVersion = "12.10.0"
+  private val FlywayVersion = "12.11.0"
 
   lazy val http4sDsl = "org.http4s" %% "http4s-dsl" % Http4sVersion
 
@@ -49,7 +49,7 @@ object Dependencies
 
   lazy val flywayPostgresql = "org.flywaydb" % "flyway-database-postgresql"  % FlywayVersion
 
-  lazy val postgresql = "org.postgresql" % "postgresql" % "42.7.12"
+  lazy val postgresql = "org.postgresql" % "postgresql" % "42.7.13"
 
   lazy val h2 = "com.h2database" % "h2" % "2.4.240"
 
@@ -73,7 +73,7 @@ object Dependencies
 
   lazy val redisTestContainer = "com.redis" % "testcontainers-redis" % "2.2.4"
 
-  lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.5.37"
+  lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.5.38"
 
   lazy val logstashLogbackEncoder = "net.logstash.logback" % "logstash-logback-encoder" % "9.0"
 
@@ -89,7 +89,7 @@ object Dependencies
 
   lazy val pegdown = "org.pegdown" % "pegdown" % "1.6.0"
 
-  lazy val sentry = "io.sentry" % "sentry" % "8.46.0"
+  lazy val sentry = "io.sentry" % "sentry" % "8.48.0"
 
   lazy val perceptualHash = "dev.brachtendorf" % "JImageHash" % "1.0.0"
 
