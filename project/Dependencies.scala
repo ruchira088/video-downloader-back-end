cd09d4296bd37eda5218c0db46778a46c8596bdd
Ruchira (Richie) Jayasekara <me@ruchij.com>
2026-07-25 19:06:49 +1000

    Bump dependencies: Flyway to 13.0.0, Apache Tika to 3.3.2, Logback to 1.6.0, and Sentry to 8.50.1.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies
   val ScalaVersion = "2.13.18"
   private val Http4sVersion = "0.23.36"
   private val CirceVersion = "0.14.16"
-  private val FlywayVersion = "12.11.0"
+  private val FlywayVersion = "13.0.0"
 
   lazy val http4sDsl = "org.http4s" %% "http4s-dsl" % Http4sVersion
 
@@ -53,7 +53,7 @@ object Dependencies
 
   lazy val h2 = "com.h2database" % "h2" % "2.4.240"
 
-  lazy val apacheTika = "org.apache.tika" % "tika-core" % "3.3.1"
+  lazy val apacheTika = "org.apache.tika" % "tika-core" % "3.3.2"
 
   lazy val redis4CatsEffects = "dev.profunktor" %% "redis4cats-effects" % "2.0.5"
 
@@ -73,7 +73,7 @@ object Dependencies
 
   lazy val redisTestContainer = "com.redis" % "testcontainers-redis" % "2.2.4"
 
-  lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.5.38"
+  lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.6.0"
 
   lazy val logstashLogbackEncoder = "net.logstash.logback" % "logstash-logback-encoder" % "9.0"
 
@@ -89,7 +89,7 @@ object Dependencies
 
   lazy val pegdown = "org.pegdown" % "pegdown" % "1.6.0"
 
-  lazy val sentry = "io.sentry" % "sentry" % "8.49.0"
+  lazy val sentry = "io.sentry" % "sentry" % "8.50.1"
 
   lazy val perceptualHash = "dev.brachtendorf" % "JImageHash" % "1.0.0"
 
